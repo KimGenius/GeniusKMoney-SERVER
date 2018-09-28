a5d605a622a1fd6517a13aa6de97f65dd8d616fb
GeniusK <geniusk1047@naver.com>
2018-09-28 12:51:59 +0900

    Database class 추가

---
+++ Database.py
@@ -0,0 +1,19 @@
+import pymysql
+
+
+class GeniusKMoney:
+    def __init__(self):
+        host = "127.0.0.1"
+        user = "root"
+        password = "root"
+        db = "geniusk_money"
+
+        self.con = pymysql.connect(host=host, user=user, password=password, db=db,
+                                   cursorclass=pymysql.cursors.DictCursor)
+        self.cur = self.con.cursor()
+
+    def list_customers(self):
+        self.cur.execute("SELECT * FROM customers")
+        result = self.cur.fetchall()
+
+        return result
